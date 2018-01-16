533c1c654fc148338fc34d414840ce6fa8c9c52d
Joe Porto <cthulhu0@gmail.com>
2018-01-16 11:13:32 -0500

    Test

---
+++ main.js
@@ -0,0 +1,47 @@
+//requirements
+var fs = require('fs');
+var http = require('http');
+//var router = require('router')
+
+//global vars
+const port=8888;
+const sitename="my awesome site";
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(renderHeader({}));
+    res.write('<h1>something!</h1>')
+    res.write(renderFooter());
+    res.end();
+    
+}).listen(port);
+
+function renderHeader(params){
+    var default_args = {
+        'title':sitename,
+        'cssClass':"",
+			
+	}
+	for(let index in default_args) {if(typeof params[index] == "undefined") params[index] = default_args[index];}
+    
+   return ['<!doctype html>',
+    '<html>',
+    '<head>',
+    '<meta charset="utf-8">',
+    '<title>'+params.title+'</title>',
+    '</head>',
+    '<body>']
+    .join('');
+    
+}
+function renderFooter(){
+    return [
+    '<footer>&copy; 2018</footer>',
+    '</body>',
+    '</html>']
+    .join('');
+}
+
+
+console.log('  Press CTRL-C to stop\n');
+
